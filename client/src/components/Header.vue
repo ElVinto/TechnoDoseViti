ce184c177f1fc5723ad20a80a7885067d4569b53
Vincent Armant <vincent.armant@insight-centre.org>
2020-09-28 15:52:10 +0200

    updates header imgs

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -6,7 +6,7 @@
     <div  v-if="$store.getters.getCurrentNavPath==='/IntraParcel'" class="header" style="background-image:url(images/bandeau_page_variabilite_intraparcellaire.jpg) ; background-size: 100% 150px;"></div>
     
     <div  v-if="$store.getters.getCurrentNavPath==='/CompCaract'" class="header" style="background-image:url(images/bandeau_page_variabilite_interparcellaire.jpg) ; background-size: 100% 150px;"></div>
-    <div  v-if="$store.getters.getCurrentNavPath==='/InterParcel'" class="header" style="background-image:url(images/bandeau_page_variabilite_interparcellaire.jpg) ; background-size: 100% 100%;"></div>
+    <div  v-if="$store.getters.getCurrentNavPath==='/InterParcel'" class="header" style="background-image:url(images/bandeau_page_variabilite_interparcellaire_cut.jpg) ; background-size: 100% 100%;"></div>
 
     <!-- <div  v-if="$store.getters.getCurrentNavPath==='/InterParcel'" class="row"  >
         <img class="col-md-12 img-fluid" src="images/bandeau_page_variabilite_interparcellaire_cut.jpg"  style="height:150px"  >
